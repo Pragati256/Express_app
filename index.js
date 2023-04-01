3d7a78568e5e430b538c1bf0aa0e4233947c1dca
pragati chaudhary <pragatichaudhary23@gmail.com>
2023-04-01 14:55:03 +0545

    This is a first node app i am creating

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = 23;
+
+// if (app > 34) {
+//   console.log("app is greater than 34");
+// } else {
+//   console.log("app is not greater than 34");
+// }
+
+// console.log("hey it is my first demo app");
+
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log(process);
